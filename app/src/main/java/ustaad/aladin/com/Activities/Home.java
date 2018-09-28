d23f787e64edf94979ba22a8c0561e1e4f5fc0cd
ingenioupkarsalan <ingeniou.aarsalan@gmail.com>
2018-09-28 17:51:47 +0500

    update

---
+++ app/src/main/java/ustaad/aladin/com/Activities/Home.java
@@ -0,0 +1,15 @@
+package ustaad.aladin.com.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ustaad.aladin.com.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/ustaad/aladin/com/Adaptors/Adaptor_for_Home_Category.java
@@ -0,0 +1,4 @@
+package ustaad.aladin.com.Adaptors;
+
+public class Adaptor_for_Home_Category {
+}
