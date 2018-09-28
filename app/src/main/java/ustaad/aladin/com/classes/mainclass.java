1c1aad267ba6671f1534418911b320c7e571574d
ingenioupkarsalan <ingeniou.aarsalan@gmail.com>
2018-09-28 17:42:39 +0500

    update

--- app/src/main/java/ustaad/aladin/com/classes/mainclass.java
+++ app/src/main/java/ustaad/aladin/com/classes/mainclass.java
@@ -1,4 +1,19 @@
 package ustaad.aladin.com.classes;
 
-public class mainclass {
+import android.app.Application;
+import android.content.ContextWrapper;
+
+import com.pixplicity.easyprefs.library.Prefs;
+
+public class mainclass extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        new Prefs.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(getPackageName())
+                .setUseDefaultSharedPreference(true)
+                .build();
+    }
 }
