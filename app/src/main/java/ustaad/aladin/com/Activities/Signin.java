ed5f2ba6be413521a63c3747e2f7bdd4486bba82
DkKhan786 <ingenious.dilawar@gmail.com>
2018-10-15 14:14:05 +0500

    Splash Screen

--- app/src/main/java/ustaad/aladin/com/Activities/Signin.java
+++ app/src/main/java/ustaad/aladin/com/Activities/Signin.java
@@ -1,51 +1,41 @@
 package ustaad.aladin.com.Activities;
 
-import android.os.Handler;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RelativeLayout;
 
 import ustaad.aladin.com.R;
+import ustaad.aladin.com.classes.Animation;
 
 public class Signin extends AppCompatActivity {
-    RelativeLayout rellay1, rellay2;
-    Button signup, loginbtn, forgetbtn;
+
+    Button signupbtn, loginbtn, forgetbtn;
     EditText email,password;
 
-
-
-    Handler handler = new Handler();
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            rellay1.setVisibility(View.VISIBLE);
-            rellay2.setVisibility(View.VISIBLE);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        signupbtn = findViewById(R.id.sing_up_btn);
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signin.this, Signup.class));
+                Animation.slideUp(Signin.this);
+            }
+        });
 
-        //take Layout
-        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
-        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
-
-        //take button
-        loginbtn = findViewById(R.id.loginbtn);
-        forgetbtn = findViewById(R.id.forgetpassbtn);
-
-        handler.postDelayed(runnable, 3000); // 3 Sec is the timeout for the splash
-
-        signup = findViewById(R.id.singbtn);
-
-        //Take Email and Pass
-        email = findViewById(R.id.emailtxt);
-        password = findViewById(R.id.passtxt);
     }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        System.exit(1);
+    }
+
 }
--- app/src/main/java/ustaad/aladin/com/Activities/Signup.java
+++ app/src/main/java/ustaad/aladin/com/Activities/Signup.java
@@ -1,15 +1,36 @@
 package ustaad.aladin.com.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import ustaad.aladin.com.R;
+import ustaad.aladin.com.classes.Animation;
 
 public class Signup extends AppCompatActivity {
 
+    Button sing_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        sing_in = findViewById(R.id.sing_in_btn);
+        sing_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this, Signin.class));
+                Animation.slideDown(Signup.this);
+            }
+        });
+    }
+
+
+    @Override
+    public void finish() {
+        super.finish();
+        Animation.slideDown(Signup.this);
     }
 }
